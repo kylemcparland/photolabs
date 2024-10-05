ffeca002dbf7354d954485bf510aaf99b4311054
Kyle McParland <kylemcparland@gmail.com>
2024-10-04 23:50:59 -0400

    Add arrayHelper to handle displaying multiple components.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,11 +14,21 @@ const sampleDataForPhotoListItem = {
   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
 };
 
+const array = [];
+
+const arrayHelper = (limit) => {
+  for (let i = 0; i < limit; i++) {
+    array.push(<PhotoListItem key={i} photoData={sampleDataForPhotoListItem}/>)
+  }
+}
+
+arrayHelper(3);
+
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <PhotoListItem photoData={sampleDataForPhotoListItem}/>
+      {array}
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -10,8 +10,10 @@ const PhotoListItem = (props) => {
       <img className="photo-list__image" src={imageSource} />
       <div className="photo-list__user-details">
         <img className="photo-list__user-profile" src={profile} />
-        <p className="photo-list__user-info">{username}</p>
-        <p className="photo-list__user-location">{location.city}, {location.country}</p>
+        <div className="photo-list__user-info">
+          <p>{username}</p>
+          <p className="photo-list__user-location">{location.city}, {location.country}</p>
+        </div>
       </div>
     </div>
   )
