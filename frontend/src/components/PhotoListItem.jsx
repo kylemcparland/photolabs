0af7ecf7cc8a2fa0fc73e5d4ededb17969339762
Kyle McParland <kylemcparland@gmail.com>
2024-10-04 22:32:33 -0400

    Add CSS styling to PhotoListItem

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
@@ -6,12 +6,14 @@ const PhotoListItem = (props) => {
   const { username, imageSource, id, location, profile } = props.photoData;
 
   return (
-    <Fragment key={id}>
-      <img src={imageSource} />
-      <img src={profile} />
-      <h2>Photographed by: {username}</h2>
-      <p>Location: {location.city} {location.country}</p>
-    </Fragment>
+    <div className="photo-list__item" key={id}>
+      <img className="photo-list__image" src={imageSource} />
+      <div className="photo-list__user-details">
+        <img className="photo-list__user-profile" src={profile} />
+        <p className="photo-list__user-info">{username}</p>
+        <p className="photo-list__user-location">{location.city}, {location.country}</p>
+      </div>
+    </div>
   )
 };
 
