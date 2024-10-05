9c815ac5ab50c68390b3de0e3b1e43ca7dcb7907
Kyle McParland <kylemcparland@gmail.com>
2024-10-04 21:47:45 -0400

    Intialize photolabs project. Create PhotoListItem component which renders data via props passed by App.jsx.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,22 @@ import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
+const sampleDataForPhotoListItem = {
+  id: "1",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+};
+
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <PhotoListItem/>
+      <PhotoListItem photoData={sampleDataForPhotoListItem}/>
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,21 +1,18 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import "../styles/PhotoListItem.scss";
 
+const PhotoListItem = (props) => {
+  const { username, imageSource, id, location, profile } = props.photoData;
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
+  return (
+    <Fragment key={id}>
+      <img src={imageSource} />
+      <img src={profile} />
+      <h2>Photographed by: {username}</h2>
+      <p>Location: {location.city} {location.country}</p>
+    </Fragment>
+  )
 };
 
 export default PhotoListItem;
