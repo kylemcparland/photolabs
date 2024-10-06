3bffdcf79f4465ae50fa21eac80a3208f8915fec
Kyle McParland <kylemcparland@gmail.com>
2024-10-06 00:00:01 -0400

    Render extended photo details in PhotoDetailsModal. Render secondary PhotoList inside PhotoDetailsModal.

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,18 +1,41 @@
 import React from 'react';
 
+import PhotoList from 'components/PhotoList';
+import photos from 'mocks/photos';
+
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
 const PhotoDetailsModal = (props) => {
-  const { displayModal, photoDetails } = props;
+  const { displayModal } = props;
 
-  console.log(photoDetails);
+  const {
+    id,
+    user: { name: username, profile },
+    urls: { full: imageSource },
+    location: { city, country },
+  } = props.photoDetails;
 
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button" onClick={() => displayModal(false)} >
         <img src={closeSymbol} alt="close symbol" />
       </button>
+      <img className="photo-details-modal__image" src={imageSource} />
+      <div className="photo-details-modal__header">
+        <div className="photo-details-modal__photographer-details">
+          <img className="photo-details-modal__photographer-profile" src={profile} />
+          <div className="photo-details-modal__photographer-info">
+            <p>{username}</p>
+            <p className="photo-details-modal__photographer-location">{city} {country}</p>
+          </div>
+        </div>
+      </div>
+      
+      <div className="photo-details-modal__images">
+      <PhotoList photos={photos} />
+      </div>
+
     </div>
   )
 };
