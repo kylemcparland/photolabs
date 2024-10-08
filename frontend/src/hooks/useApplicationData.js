3fd0bef81e6e8ec25e5a5a570405cdd50a0749c0
Kyle McParland <kylemcparland@gmail.com>
2024-10-07 20:59:27 -0400

    Refactor useApplicationData to use new reducer function in useReducer hook to handle state.

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,36 +1,68 @@
-import { useState } from "react";
-
+import { useReducer } from "react";
 import photos from "mocks/photos";
 import topics from 'mocks/topics';
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FAV_PHOTO_ADDED":
+      return {
+        ...state,
+        favPhotoArray: [...state.favPhotoArray, action.payload.id]
+      };
+    case "FAV_PHOTO_REMOVED":
+      return {
+        ...state,
+        favPhotoArray: state.favPhotoArray.filter(fav => fav !== action.payload.id)
+      };
+    case "SELECT_PHOTO":
+      return {
+        ...state,
+        modal: action.payload.selectedPhoto
+      };
+    case "CLOSE_MODAL":
+      return {
+        ...state,
+        modal: undefined
+      };
+    default:
+      return state;
+  }
+};
+
 const useApplicationData = () => {
 
-  // ==> STATE OBJECT:
-  const [state, setState] = useState({
+  // ==> INITIALIZE STATE OBJECT:
+  const initialState = {
     favPhotoArray: [],
     modal: undefined
-  });
+  };
+
+  // ==> USE-REDUCER:
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   // ==> ACTION TO SET UPDATE FAVS:
   const updateToFavPhotoIds = (id, isFav) => {
-    setState(prevState => {
-      const updatedFavs = isFav
-        ? prevState.favPhotoArray.filter(fav => fav !== id) // remove from array
-        : [...prevState.favPhotoArray, id]; // add to array
-
-      return { ...prevState, favPhotoArray: updatedFavs };
+    dispatch({
+      type: isFav ? "FAV_PHOTO_REMOVED" : "FAV_PHOTO_ADDED",
+      payload: { id }
     });
   };
 
   // ==> ACTION TO SELECT PHOTO:
   const setPhotoSelected = (id) => {
     const selectedPhoto = photos.find(photo => photo.id === id);
-    setState(prevState => ({ ...prevState, modal: selectedPhoto }))
+    dispatch({
+      type: "SELECT_PHOTO",
+      payload: { selectedPhoto }
+    })
   };
 
   // ==> ACTION TO CLOSE MODAL:
   const onClosePhotoDetailsModal = () => {
-    setState(prevState => ({ ...prevState, modal: undefined }));
+    dispatch({
+      type: "CLOSE_MODAL",
+      payload: {}
+    })
   };
 
   return {
